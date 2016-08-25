49ecd67a4c0c25427de1804b524b1bb3b78c991e
Dale S Hendrickson <dale@dshendrickson.com>
2016-08-25 13:12:27 -0600

    Cleaned up javascript.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,52 @@
-// //** Local Functions
-// function getRandomNum (){
-//   randomNumber =  Math.floor(Math.random() * (100 + 1) - 1);
-//   return randomNumber;
-// };
+//** Number Guesser
+// Phase One: Basic Requirements
+//
+// The application should have the following user interface components:
+//
+// An input field for guessing the number
+// A button for submitting a guess
+// A button for clearing the input field
+// A button that resets the game to its initial state
+// An element that displays the users most recent guess
+// An element that displays a message to user
+// If their guess is too high, it should display: “Sorry, that guess is too high. Try a lower number.”
+// If their guess is too low, it should display: “Sorry, that guess is too low. Try a higher number.”
+// Some things to think about:
+//
+// Input fields—regardless of their “type” attribute—store their values in strings. You’ll need to use parseInt() to turn it back into a number.
+// You’ll need a strategy for generating random numbers.
 
-//** Development and Testing Variables
-var h1 = document.querySelector('h1');
-var h3 = document.querySelector('h3');
+// Phase Two: More Better
+//
+// The type attribute for the guess input should be “number”.
+// It should have “min” and a “max” attributes equal to the minimum and maximum of the reange of guesses (e.g. 1 and 100, respectively).
+// The application should display an error message if the guess is not a number (e.g. parseInt() returns NaN).
+// The application should display an error if the guess is outside of the range of possible answers.
+// The clear button should be disabled if there is nothing to clear.
+
+// The reset button should be disabled if there is nothing to reset.
+// Phase Three: Level Up
+//
+// Add additional inputs that allows the user to specify what the minimum and maximum numbers should be.
+// Every time the user wins a round increase the maximum number by 10.
+// Every time the user wins a round decrease the minimum number by 10.
+// (Pro-tip: You’ll need to adjust the input fields to accept the new minimum and maximum numbers.)
+//
+// Rubric
+//
+// Functional Expectations
+//
+// 4: Application meets all of the functional expectations in Phase Three
+// 3: Application meets all of the functional expectations in Phase Two
+// 2: Application meets all of the functional expectations in Phase One
+// 1: Application does not meet the requirements in Phase One
+// Fundamental JavaScript Style
+//
+// 4: Application demonstrates excellent knowledge of JavaScript syntax, style, and refactoring
+// 3: Application shows strong effort towards organization, content, and refactoring
+// 2: Application runs but the code has long functions, unnecessary or poorly named variables, and needs significant refactoring
+// 1: Application generates syntax error or crashes during execution
+
 
 //** User Input Varialbles
 var userInput = document.querySelector('#user-input');
@@ -23,6 +63,8 @@ var newMax = '';
 //** Tag Variables
 var guessResponse = document.querySelector('.guess-response');
 var lastGuess = document.querySelector('.last-guess')
+var h1 = document.querySelector('h1');
+var h3 = document.querySelector('h3');
 
 //** Button Variables
 var guessButton = document.querySelector('.guess-button');
@@ -32,12 +74,6 @@ var rangeButton = document.querySelector('.range-button');
 
 //** Event fucntions
 
-rangeButton.addEventListener('click', function () {
-  inputMin = parseInt(inputMin.value);
-  inputMax = parseInt(inputMax.value);
-  randomNumber = getRandomNum();
-});
-
 userInput.addEventListener('keyup', function () {
   if(userInput.value !== '') {
     activateClear();
@@ -46,13 +82,6 @@ userInput.addEventListener('keyup', function () {
     deactivateClear();
   }
 });
-
-// rangeButton.addEventListener('click', function () {
-//   h1.innerText = 'I AM A GUESS!!!';
-//
-//   var inputMin = document.querySelector('#input-min').value;
-//   var inputMax = document.querySelector('#input-max').value;
-// });
 
 guessButton.addEventListener('click', function () {
   var userInput = document.querySelector('#user-input');
@@ -66,13 +95,13 @@ guessButton.addEventListener('click', function () {
   if (isNaN(userNumber)) {
     h3.innerText = "Entry must be an integer.";
     resetGuessResponse ();
+    activateReset ();
   } else if (userNumber <= inputMax && userNumber >= inputMin) {
-
-    returnGuessResponse ();
-
+      returnGuessResponse ();
   } else {
-    h3.innerText = "Please pick a number between " +  inputMin + " and " + inputMax;
-    resetGuessResponse ();
+      h3.innerText = "Please pick a number between " +  inputMin + " and " + inputMax;
+      resetGuessResponse ();
+      activateReset ();
   };
 });
 
@@ -84,12 +113,13 @@ clearButton.addEventListener('click', function () {
 });
 
 resetButton.addEventListener('click', function() {
-  h3.innerText ='?';
-  resetInputValue ();
-  resetGuessResponse ();
-  deactivateClear ();
-  deactivateReset ();
-  getRandomNum ();
+  location.reload ();
+});
+
+rangeButton.addEventListener('click', function () {
+  var inputMin = document.querySelector('#input-min');
+  var inputMax = document.querySelector('#input-max');
+  randomNumber = Math.floor((Math.random() * (inputMax.value - inputMin.value + 1)) + inputMin.value);
 });
 
 //** Local Functions
@@ -98,9 +128,9 @@ function returnGuessResponse () {
   activateReset();
   if (userNumber === randomNumber) {
     guessResponse.innerText = "You Win!";
-
-
-
+    inputMin = parseInt(inputMin.value) - 10;
+    inputMax = parseInt(inputMax.value) + 10;
+    randomNumber = Math.floor((Math.random() * (((inputMax.value) - (inputMin.value) + 1))) + (inputMin.value));
   } else if (userNumber > randomNumber) {
     guessResponse.innerText = "Sorry, that guess is too high. Try another number."
   } else {
